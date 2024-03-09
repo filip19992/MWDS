e80dcd28120f0c1a31a0a1bc2ab662c247da251f
filip19992 <filipwlodarczyk99@gmail.com>
2024-03-09 08:29:49 +0100

    abc

--- APP/Model/MwdsfwContext.cs
+++ APP/Model/MwdsfwContext.cs
@@ -19,7 +19,7 @@ public partial class MwdsfwContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=tcp:mwdsfw.database.windows.net,1433;Initial Catalog=mwdsfw;Persist Security Info=False;User ID=wlofi001;Password=Gliniana8???;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        => optionsBuilder.UseSqlServer();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
